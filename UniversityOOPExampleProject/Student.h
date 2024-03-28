2a14ff90bade45ffb91772ff2a4dd8e1407832e3
ITSTEP\st <st>
2024-03-28 18:22:11 +0300

    Add project files.

---
+++ UniversityOOPExampleProject/Student.h
@@ -0,0 +1,52 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Student {
+public:
+	string name;
+	int age;
+	double mark;
+
+	// default constructor
+	Student() {
+		//cout << "calling default constructor" << endl;
+		name = "no name";
+		age = 5;
+		mark = 4;
+	}
+
+	// canonical constructor with parameters (arguments)
+	Student(string nm, int a, double m) {
+		//cout << "calling canonical constructor" << endl;
+		name = nm;
+		age = a;
+		mark = m;
+	}
+
+	// constructor with parameters (arguments)
+	Student(string nm) {
+		//cout << "calling canonical constructor" << endl;
+		name = nm;
+		age = 0;
+		mark = 0;
+	}
+
+	// constructor with parameters (arguments)
+	Student(double m) {
+		//cout << "calling canonical constructor" << endl;
+		name = "no name";
+		age = 0;
+		mark = m;
+	}
+
+	string convert() {
+		string s = "";
+		s += name;
+		s += ": age = " + to_string(age);
+		s += ", mark = " + to_string(mark);
+		return s;
+	}
+};
---
+++ UniversityOOPExampleProject/main.cpp
@@ -0,0 +1,17 @@
+#include "Student.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	//Student st1("Alex", 14, 9);
+	//Student st2("Alice", 13, 7);
+	//Student st3("Potter", 15, 10);
+	Student st4;
+
+	//cout << st1.convert() << endl;
+	//cout << st2.convert() << endl;
+	//cout << st3.convert() << endl;
+	cout << st4.convert() << endl;
+
+	return 0;
+}
